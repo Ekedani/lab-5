c4c5597b89b4cea0883de50644a2eb1e6140924b
Vladlen <ilusenkovladlen@gmail.com>
2021-05-03 12:53:25 +0300

    Создана заготовка для работы

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
+#include "r-tree/r_tree.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
---
+++ r-tree/r_tree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User on 03.05.2021.
+//
+
+#include "r_tree.h"
