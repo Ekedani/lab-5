c144f32b584bc47fd7a9d6d9a22844ee3c285e84
koziuberda <koziuberdaseverin@gmail.com>
2021-05-06 23:42:49 +0300

    Реализован chooseSubtree

--- r-tree/r_tree.cpp
+++ r-tree/r_tree.cpp
@@ -1,5 +1,5 @@
 #include "r_tree.h"
-
+#include <vector>
 Node* rTree::chooseSubtree(Place new_place) {
     return chooseSubtree(root, new_place);
 }
@@ -9,31 +9,25 @@ Node* rTree::chooseSubtree(Node *start, Place new_place) {
     if (start->isLeaf()){
         return start;
     } else {
-        bool thereIsNoLeaves = true;
-        double minimal_overlap_area = 999999999;
         int index;
+        Point point (new_place.longitude,new_place.latitude);
+        double minimal_mbr_increasing_area = 999999999;
+        std::vector<int> chosen_nodes;
         for (int i = 0; i < start->nodes.size(); ++i) {
-            if (start->nodes[i].isLeaf()){
-                thereIsNoLeaves = false;
-                //TODO: выбрать из листков тот, у которого наименьшим образом возрастает пересечение
-                //Я не очень понял что это значит. Когда разберусь - напишу.
+            if (start->nodes[i].MBR.HowMuchIncreasesTheArea(point) < minimal_mbr_increasing_area){
+                minimal_mbr_increasing_area = start->nodes[i].MBR.HowMuchIncreasesTheArea(point);
             }
         }
-
-        //выбираем дочерний узел с наименьшим увеличением площади MBR
-        if (thereIsNoLeaves) {
-            Point point (new_place.longitude,new_place.latitude);
-            double minimal_mbr_area = 999999999;
-            //index определен раньше. По хорошему, надо хранить все ноды, которые достигнут минимума...
-            //но мне лень. может позже вектор приделаю
-            for (int i = 0; i < start->nodes.size(); ++i) {
-                if (start->nodes[i].MBR.HowMuchIncreasesTheArea(point) < minimal_mbr_area){
-                    minimal_mbr_area = start->nodes[i].MBR.HowMuchIncreasesTheArea(point);
+        double minimal_area = 999999999;
+        for (int i = 0; i < start->nodes.size(); ++i) {
+            if (start->nodes[i].MBR.HowMuchIncreasesTheArea(point) == minimal_mbr_increasing_area) {
+                if (start->nodes[i].MBR.area() < minimal_area) {
+                    minimal_area = start->nodes[i].MBR.area();
                     index = i;
                 }
             }
-            chosen = &start->nodes[index];
         }
+        chosen = &start->nodes[index];
     }
     
     return chooseSubtree(chosen, new_place);
